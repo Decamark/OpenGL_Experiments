9ad9cd442a3cdc9a4b4a450aec316484c06706ed
Decamark <decamark@tutanota.com>
2021-12-12 16:59:35 +0900

    Moved 09 into math

--- lab/math/09_geometry_transformation.cpp
+++ lab/math/09_geometry_transformation.cpp
@@ -1,18 +1,18 @@
 /**
- * 09: rotation * translation
+ * (Math) 09: rotation * translation
  */
 
 #include <functional>
 
-#include <gle/gle.hpp>
-#include <gle/export.hpp>
-#include <gle/shape.hpp>
-#include <gle/grid.hpp>
-#include <gle/time.hpp>
+#include <glab/glab.hpp>
+#include <glab/shape.hpp>
+#include <glab/grid.hpp>
+#include <glab/time.hpp>
+
 #include <learnopengl/shader_m.h>
 #include <learnopengl/camera.h>
 
-auto* window = gle::setup(1200, 800, "Practice");
+auto* window = glab::setup(1200, 800, "glab");
 Camera camera(window, glm::vec3(50.0f, 50.0f, 50.0f), glm::vec3(0.0f, 1.0f, 0.0f), -135.0f, -30.0f);
 
 int main()
@@ -29,40 +29,40 @@ int main()
 
   glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1200.0f / 800.0f, 0.1f, 100.0f);
 
-  gle::Grid grid(100.0f);
+  glab::Grid grid(100.0f);
   grid.setP(projection);
 
-  gle::Tetragon tetra1(10.0f, 10.0f, 15.0f, 15.0f, 15.0f);
+  glab::Tetragon tetra1(10.0f, 10.0f, 15.0f, 15.0f, 15.0f);
   tetra1.setP(projection);
 
-  gle::Tetragon tetra2(10.0f, 10.0f, 30.0f, 15.0f, 15.0f);
+  glab::Tetragon tetra2(10.0f, 10.0f, 30.0f, 15.0f, 15.0f);
   tetra2.setP(projection);
 
-  gle::Tetragon tetra3(10.0f, 10.0f, 45.0f, 15.0f, 15.0f);
+  glab::Tetragon tetra3(10.0f, 10.0f, 45.0f, 15.0f, 15.0f);
   tetra3.setP(projection);
 
-  gle::Tetragon tetra4(10.0f, 10.0f, 60.0f, 15.0f, 15.0f);
+  glab::Tetragon tetra4(10.0f, 10.0f, 60.0f, 15.0f, 15.0f);
   tetra4.setP(projection);
 
-  gle::Tetragon tetra5(10.0f, 10.0f, 15.0f, 30.0f, 15.0f);
+  glab::Tetragon tetra5(10.0f, 10.0f, 15.0f, 30.0f, 15.0f);
   tetra5.setP(projection);
   // tetra5.translate({-0.0f, -30.0f, -15.0f});
-  // gle::debug(tetra5.model);
+  // glab::debug(tetra5.model);
   // std::cout << "-----" << std::endl;
   // tetra5.rotateAround(90.0f, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 0.0f});
-  // gle::debug(tetra5.model);
+  // glab::debug(tetra5.model);
   // std::cout << "-----" << std::endl;
   // tetra5.translate({10.0f, 30.0f, 0.0f});
-  // gle::debug(tetra5.model);
+  // glab::debug(tetra5.model);
   // tetra5.translate({0.0f, 30.0f, 15.0f});
 
-  gle::Tetragon tetra6(10.0f, 10.0f, 30.0f, 30.0f, 15.0f);
+  glab::Tetragon tetra6(10.0f, 10.0f, 30.0f, 30.0f, 15.0f);
   tetra6.setP(projection);
 
-  // gle::Tetragon tetra7(10.0f, 10.0f, 45.0f, 30.0f, 15.0f);
+  // glab::Tetragon tetra7(10.0f, 10.0f, 45.0f, 30.0f, 15.0f);
   // tetra7.setP(projection);
 
-  gle::Clock clock;
+  glab::Clock clock;
   while (!glfwWindowShouldClose(window))
   {
     float dt = clock.tick();
@@ -78,7 +78,7 @@ int main()
     tetra1.setV(camera.GetViewMatrix());
     tetra1.rotate(glm::radians(10.0f), {0.0f, 1.0f, 0.0f});
     tetra1.draw();
-    tetra1.draw_guide();
+    tetra1.drawGuide();
 
     // Buggy rotation
     tetra2.setV(camera.GetViewMatrix());
@@ -99,14 +99,14 @@ int main()
     tetra2.model = rot * tetra2.model;
     tetra2.setM();
     tetra2.draw();
-    tetra2.draw_guide();
+    tetra2.drawGuide();
 
     // Rotate on the spot
     tetra3.setV(camera.GetViewMatrix());
     tetra3.model = tetra3.model * rot;
     tetra3.setM();
     tetra3.draw();
-    tetra3.draw_guide();
+    tetra3.drawGuide();
 
     // Rotate around the given point (y-axis)
     tetra4.setV(camera.GetViewMatrix());
@@ -115,7 +115,7 @@ int main()
     tetra4.translate({5.0f, 0.0f, 5.0f});
     tetra4.setM();
     tetra4.draw();
-    tetra4.draw_guide();
+    tetra4.drawGuide();
 
     // Rotate around the given point (freely)
     tetra5.setV(camera.GetViewMatrix());
@@ -123,12 +123,12 @@ int main()
     tetra5.rotate(90.0f*dt, {1.0f, 1.0f, 0.0f});
     tetra5.translate({-10.0f, -10.0f, -10.0f});
     tetra5.draw();
-    tetra5.draw_guide();
+    tetra5.drawGuide();
 
     tetra6.setV(camera.GetViewMatrix());
     tetra6.rotateAround(90.0f*dt, {0.0f, 1.0f, 1.0f}, {5.0f, 5.0f, 5.0f});
     tetra6.draw();
-    tetra6.draw_guide();
+    tetra6.drawGuide();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
