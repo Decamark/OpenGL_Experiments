dd52de1977cc800283b5e65a9ac78852c25628a9
Decamark <decamark@tutanota.com>
2021-12-22 22:34:02 +0900

    misc:05 dirty implementation

--- include/glab/shape/polyhedron.hpp
+++ include/glab/shape/polyhedron.hpp
@@ -42,6 +42,34 @@ namespace glab
 
       setPos(x, y, z);
     }
+
+    Cube(float x, float y, float z, float w, glm::vec3 c) : x0(x), y0(y), z0(z)
+    {
+      primitive = GL_TRIANGLES;
+
+      std::vector<float> vertices = {
+        -w/2,  w/2,  w/2, c.r, c.g, c.b,
+        -w/2, -w/2,  w/2, c.r, c.g, c.b,
+         w/2,  w/2,  w/2, c.r, c.g, c.b,
+         w/2, -w/2,  w/2, c.r, c.g, c.b,
+        -w/2,  w/2, -w/2, c.r, c.g, c.b,
+        -w/2, -w/2, -w/2, c.r, c.g, c.b,
+         w/2,  w/2, -w/2, c.r, c.g, c.b,
+         w/2, -w/2, -w/2, c.r, c.g, c.b
+      };
+      std::tie(vao,vbo) = partition(vertices, 2, 3, 3);
+      std::vector<unsigned int> indices = {
+        4, 0, 6, 6, 0, 2, // v4-v0-v2-v6
+        0, 1, 2, 2, 1, 3, // v0-v1-v3-v2
+        2, 3, 6, 6, 3, 7, // v2-v3-v7-v6
+        6, 7, 4, 4, 7, 5, // v6-v7-v5-v4
+        4, 5, 0, 0, 5, 1, // v4-v5-v1-v0
+        1, 3, 5, 7, 5, 3  // v1-v5-v7-v3
+      };
+      ebo = order(vao, indices);
+
+      setPos(x, y, z);
+    }
   }; // class Cube
 
   // http://www.songho.ca/opengl/gl_sphere.html
--- lab/misc/05_interaction.cpp
+++ lab/misc/05_interaction.cpp
@@ -7,9 +7,21 @@
 #include <glab/grid.hpp>
 #include <glab/time.hpp>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/norm.hpp>
+
 #include <learnopengl/camera.h>
 
-glm::vec3 Front = glm::vec3(0.0f, 0.0f, -1.0f);
+const float PI = glm::pi<float>();
+glm::vec3 ray = glm::vec3(0.0f);
+// We cannot use glad-related APIs before calling initWindow, so just declare a pointer here
+glab::Cube* cube;
+glm::vec3 axis = {1.0f, 1.0f, 1.0f};
+float theta = 0.0f;
+glm::vec3 Front = {0.0f, 0.0f, 1.0f}, Right = {1.0f, 0.0f, 0.0f}, Up = {0.0f, 1.0f, 0.0f};
 
 // Convert a coordinate in screen space to the one in eye space
 std::pair<double,double> screen2eye(double xpos, double ypos)
@@ -23,17 +35,38 @@ std::pair<double,double> screen2eye(double xpos, double ypos)
   return {xe, ye};
 }
 
-// Joey calculates yaw and pitch in the "screen space" to implement Camera.
-// ref: https://learnopengl.com/Getting-started/Camera
-// This works fine if the cursor's position doesn't matter
-// but I wanted to judge the intersection between an object and the cursor
-// and with Joey's method, it looks weird because the point of the Front vector
-// isn't covered with the cursor!
-// So, I needed to handle the cursor as if it's in the "eye space" to make it look more natural.
+// Gets called whenever the cursor moves
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
+  // Joey calculates yaw and pitch in the "screen space" to implement Camera.
+  // ref: https://learnopengl.com/Getting-started/Camera
+  // This works fine if the cursor's position doesn't matter
+  // but I wanted to judge the intersection between an object and the cursor
+  // and with Joey's method, it looks weird because the point of the ray vector
+  // isn't covered with the cursor!
+  // So, I needed to handle the cursor as if it's in the "eye space" to make it look more natural.
   auto [xe,ye] = screen2eye(xpos, ypos);
-  Front = {xe, ye, -0.1f};
+  ray = {xe, ye, -0.1f};
+
+  static int shouldMove = false;
+  static float lastX, lastY;
+
+  if (shouldMove)
+  {
+    glm::vec3 l = Right * (float) (xe-lastX) + Up * (float) (ye-lastY);
+    axis = glm::normalize(glm::cross(l, Front));
+    float distance = glm::l2Norm(glm::vec3(cube->model[0][3], cube->model[1][3], cube->model[2][3])-(*camera).Position);
+    theta = -glm::degrees( glm::atan(glm::l2Norm(l) * distance / 0.1f) );
+  }
+
+  int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+  if (state == GLFW_PRESS and cube->intersect((*camera).Position, ray))
+    shouldMove = true;
+  else
+    shouldMove = false;
+
+  lastX = xe;
+  lastY = ye;
 }
 
 int main()
@@ -46,7 +79,7 @@ int main()
   camera0 |= projection;
   camera  |= camera0;
 
-  glab::Cube cube(0.0f, 0.0f, 0.0f, 5.0f);
+  cube = new glab::Cube(0.0f, 0.0f, 0.0f, 5.0f);
 
   glab::clock.start();
   while (!glfwWindowShouldClose(window))
@@ -55,15 +88,31 @@ int main()
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glab::Line ray((*camera).Position+glm::vec3(0.0f, 0.0f, -0.1f), {1.0f, 0.0f, 0.0f},
-                   (*camera).Position+Front,                        {0.0f, 1.0f, 0.0f});
-    ray.draw();
+    glab::Line beam((*camera).Position+glm::vec3(0.0f, 0.0f, -0.1f), {1.0f, 0.0f, 0.0f},
+                    (*camera).Position+ray,                          {0.0f, 1.0f, 0.0f});
+    beam.draw();
 
-    if (cube.intersect((*camera).Position, Front))
-      cube.rasterization = GL_FILL;
+    Front = glm::rotate(Front, glm::radians(-theta), axis);
+    Right = glm::rotate(Right, glm::radians(-theta), axis);
+    Up    = glm::rotate(   Up, glm::radians(-theta), axis);
+
+    cube->rotate(theta, axis);
+
+    // glab::Line front(glm::vec3(0.0f, 0.0f, 0.0f), {1.0f, 1.0f, 0.0f}, Front*5.0f, {1.0f, 1.0f, 0.0f}); // yellow
+    glab::Cube front(Front.x*3, Front.y*3, Front.z*3, 1.0f, {1.0f, 1.0f, 0.0f});
+    // glab::Line right(glm::vec3(0.0f, 0.0f, 0.0f), {0.0f, 1.0f, 1.0f}, Right*5.0f, {0.0f, 1.0f, 1.0f}); // cyan
+    glab::Cube right(Right.x*3, Right.y*3, Right.z*3, 1.0f, {0.0f, 1.0f, 1.0f});
+    // glab::Line    up(glm::vec3(0.0f, 0.0f, 0.0f), {1.0f, 0.0f, 1.0f},    Up*5.0f, {1.0f, 0.0f, 1.0f}); // magenta
+    glab::Cube up(Up.x*3, Up.y*3, Up.z*3, 1.0f, {1.0f, 0.0f, 1.0f});
+    front.draw();
+    right.draw();
+       up.draw();
+
+    if (cube->intersect((*camera).Position, ray))
+      cube->rasterization = GL_FILL;
     else
-      cube.rasterization = GL_LINE;
-    cube.draw();
+      cube->rasterization = GL_LINE;
+    cube->draw();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
